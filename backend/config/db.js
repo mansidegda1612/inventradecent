2e34dd029eb0c28e13c7f0c02b8965700d9cdd1d
mansidegda1612 <mansidegda@gmail.com>
2026-06-13 23:02:04 +0530

    db.js changes tor tidb

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,14 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: process.env.DB_PORT,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+   ssl: {
+    minVersion: "TLSv1.2",
+    rejectUnauthorized: true,
+  },
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
