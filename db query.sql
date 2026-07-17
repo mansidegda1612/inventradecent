02fa488248575f91f2f2028d0e6f698504a00546
mansidegda1612 <mansidegda@gmail.com>
2026-07-17 22:52:19 +0530

    new branch for multi-tenant

---
+++ db query.sql
@@ -0,0 +1,6 @@
+/*
+-- Query: SELECT * FROM inventradecent.user
+-- Date: 2026-06-12 23:55
+*/
+INSERT INTO `` (`id`,`user_id`,`password`,`name`,`userrole`,`rights`) VALUES (1,'admin','$2b$10$GFv2PlPlubZmvOQGPwbdS.L9ZSP11SSLG5yRLAVgTRiOLikJe0ihe','Admin',1,NULL);
+INSERT INTO `` (`id`,`user_id`,`password`,`name`,`userrole`,`rights`) VALUES (2,'md','$2a$10$s/WbEiaUb4zM6R9pdmREkuSsJtqAyg1V2bC0qGK//fFPJMMA55W9S','md',1,NULL);
